52eced4bc244204fddc5d7f59f5b458448907523
flxkrlv <72403830+flxkrlv@users.noreply.github.com>
2021-02-18 12:47:08 +0300

    1

--- loading/script.js
+++ loading/script.js
@@ -1,3 +1,3 @@
-setTimeout(function(){
-    window.location.href = 'https://limondevelopers.github.io/limon/main/';
-  }, 5 * 1000);
+//setTimeout(function(){
+//  window.location.href = 'https://limondevelopers.github.io/limon/main/';
+//  }, 5 * 1000);
