835c82089ef7b95f78060f1c489e052d58389f0f
flxkrlv <flxkrlv@yandex.ru>
2021-03-04 23:18:59 +0300

    1

--- loading/script.js
+++ loading/script.js
@@ -1,3 +1,2 @@
-//setTimeout(function(){
-//  window.location.href = 'https://limondevelopers.github.io/limon/main/';
-//  }, 5 * 1000);
+setTimeout(function(){
+  window.location.href="../main/index.html";}, 5 * 1000);
